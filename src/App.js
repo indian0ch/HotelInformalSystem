5916f5d89223a5b1ae1e1bb8af52ed9db61438fb
AndriykoF <fesiukandrey@gmail.com>
2023-05-23 22:47:12 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+
+import styled from './App.module.css';
+
+const App = () => {
+
+  return (
+    <div>
+      <section className={styled.goalform}>
+
+      </section>
+      <section className={styled.goal}>
+
+      </section>
+    </div>
+  );
+};
+
+export default App;
