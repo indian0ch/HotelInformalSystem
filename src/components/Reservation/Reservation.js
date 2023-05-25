2faa6d04fd5fa37a2062bb9d0e342cc2e349c0ae
AndriykoF <fesiukandrey@gmail.com>
2023-05-25 17:32:51 +0300

    Reservation front-end part was done)

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -1,22 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Reservation.module.css";
-import { Room } from './ReservationStrategy.js';
-
+import {
+  Room,
+  StandartReservation,
+  MiddleReservation,
+  LuxuryReservation,
+} from "./ReservationStrategy.js";
+import Button from "../UI/Button/Button.js";
 
 const Reservation = (props) => {
-    const room=new Room();
+  const [priceRoom, setPriceRoom] = useState("100");
+  const [nameUser, setNameUser] = useState("");
+  const [surnameUser, setSurnameUser] = useState("");
+  const [idUser, setIDUser] = useState("");
+  const [cityUser, setCityUser] = useState("");
+  function onChangeName(event) {
+    setNameUser(event.target.value);
+  }
+  function onChangeSurname(event) {
+    setSurnameUser(event.target.value);
+  }
+  function onChangeCity(event) {
+    setIDUser(event.target.value);
+  }
+  function onChangeID(event) {
+    setCityUser(event.target.value);
+  }
+  function onChangePriceHandler(event) {
+    setPriceRoom(event.target.value);
+  }
+  function onSubmitFormHandler(event) {
+    event.preventDefault();
+    let roomType = undefined;
+    switch (priceRoom) {
+      case "100":
+        roomType = new StandartReservation();
+        break;
+      case "200":
+        roomType = new MiddleReservation();
+        break;
+      case "300":
+        roomType = new LuxuryReservation();
+        break;
+    }
+    const room = new Room(surnameUser, nameUser, idUser, cityUser, roomType);
     room.Reservation();
-    // const adm=new HotelAdministration();
-    // const res=new Residents();
-    // const res1=new Residents();
-
-    //  res.Attach(adm);
-    //  res.SatisfactionLevelSet=2;
-
-    //  res1.Attach(adm);
-    //  res1.SatisfactionLevelSet=33;
-  return <div className={styles.reservationContainer}>
-      <p>Hello</p>
-  </div>
+  }
+  return (
+    <div className={styles.reservationContainer}>
+      <h2>Reservation section</h2>
+      <form onSubmit={onSubmitFormHandler}>
+        <label htmlFor="nameUser">Your Name:</label>
+        <input type="text" name="nameUser" onChange={onChangeName}></input>
+        <label htmlFor="surnameUser">Your Surname:</label>
+        <input
+          type="text"
+          name="surnameUser"
+          onChange={onChangeSurname}
+        ></input>
+        <label htmlFor="cityUser">Your city:</label>
+        <input type="text" name="cityUser" onChange={onChangeCity}></input>
+        <label htmlFor="idUser">Passport ID:</label>
+        <input type="number" name="idUser" onChange={onChangeID}></input>
+        <label htmlFor="roomUser">Room's type:</label>
+        <div>
+          <select
+            name="roomUser"
+            className={styles.sectionBox}
+            onChange={onChangePriceHandler}
+          >
+            <option value="100">Standart room</option>
+            <option value="200">Middle room</option>
+            <option value="300">Luxury room</option>
+          </select>
+          <p>Price: {priceRoom}$</p>
+        </div>
+        <Button type="submit">Reservation</Button>
+      </form>
+    </div>
+  );
 };
 export default Reservation;
