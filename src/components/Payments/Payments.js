020e5bc2057af4d837796c74abd15bd969939a57
AndriykoF <fesiukandrey@gmail.com>
2023-05-25 19:57:52 +0300

    Added typescript's logic to Payments component)

--- src/components/Payments/Payments.js
+++ src/components/Payments/Payments.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import styles from "./Payments.module.css";
-// import {
-
-// } from "./PaymentsComposite.js";
+import { Composite, poolPayment } from "./PaymentsComposite.js";
 
 const Payments = (props) => {
-
+  // const spa = new Composite(); // Pass the desired arguments to the constructor
+  // const hotel = new Composite();
+  // console.log(spa.name);
+  // const pool = new poolPayment(400);
+  // console.log(pool.amountService);
+  // const pool2 = new poolPayment(300);
+  // spa.AddComponent(pool);
+  // spa.AddComponent(pool2);
+  // console.log(spa.getAmount);
+  // spa.addPayment(33);
+  // console.log(spa.getAmount);
+  // spa.issueRefund(33);
+  // console.log(spa.getAmount);
   return (
     <div className={styles.paymentsContainer}>
       <p>Payments front-end part</p>
